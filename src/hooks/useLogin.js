95bd0ba7c51b1e47bf12bdef25265a4f7211e9c2
RehmanAli10 <vrehmanali@gmail.com>
2024-07-11 09:47:47 +0500

    created custom hook for login

---
+++ src/hooks/useLogin.js
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+
+import { login as loginApi } from "../services/apiAuth";
+import toast from "react-hot-toast";
+
+export function useLogin() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const { mutate: login, isLoading: isLoggingIn } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+
+    onSuccess: (user) => {
+      queryClient.setQueryData(["user"], user.user);
+      navigate("/app", { replace: true });
+    },
+
+    onError: (err) => {
+      toast.error("Provided email or password are incorrect");
+    },
+  });
+
+  return { login, isLoggingIn };
+}
