582f7ed12b68e1c7e8c55b5661a48542b38dffee
RehmanAli10 <vrehmanali@gmail.com>
2024-07-15 14:09:35 +0500

    custom hook for sigin with google

---
+++ src/hooks/useSignedinwithGoogle.js
@@ -0,0 +1,29 @@
+import { useQueryClient, useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+import { signinwithGoogle as signinwithGoogleApi } from "../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+
+export function useSignedinwithGoogle() {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
+  const {
+    data,
+    isLoading: isSigningin,
+    mutate: signinwithGoogle,
+    error,
+  } = useMutation({
+    mutationFn: signinwithGoogleApi,
+    onSuccess: () => {
+      toast.success("Signed in successfully");
+      queryClient.invalidateQueries({
+        queryKey: ["user"],
+      });
+      navigate("/app", { replace: true });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { isSigningin, signinwithGoogle, error, data };
+}
