b5d2e520ca976bd60a7361b2682d04e9bcefb17f
RehmanAli10 <vrehmanali@gmail.com>
2024-07-16 11:24:49 +0500

    refactored

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,22 +1,3 @@
-// import Map from "../components/Map";
-// import SideBar from "../components/SideBar";
-// import User from "../components/User";
-// import styles from "./AppLayout.module.css";
-
-// function AppLayout() {
-//   return (
-//     <div className={styles.app}>
-//       <SideBar />
-
-//       <User />
-
-//       <Map />
-//     </div>
-//   );
-// }
-
-// export default AppLayout;
-
 import Map from "../components/Map";
 import SideBar from "../components/SideBar";
 import User from "../components/User";
