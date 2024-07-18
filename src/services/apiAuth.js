1a3ce17d25638368a2ad6b181de2436924027c73
RehmanAli10 <vrehmanali@gmail.com>
2024-07-18 09:38:23 +0500

    refactored

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -48,7 +48,7 @@ export async function signinwithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `http://localhost:5173/app`,
+      redirectTo: `https://world-wise-updated.vercel.app/app`,
     },
   });
 
